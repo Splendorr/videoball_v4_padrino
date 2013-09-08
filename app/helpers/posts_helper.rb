6e7f8fcb2bebddd9fbbfd464d10593037ad51df1
Splendorr <simolinic@gmail.com>
2013-09-08 18:22:06 -0400

    "padrino g controller posts get:index get:show"

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,7 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+VideoballV4Padrino::App.helpers do
+  # def simple_helper_method
+  #  ...
+  # end
+end
